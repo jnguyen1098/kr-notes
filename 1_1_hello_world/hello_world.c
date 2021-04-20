5b4c9c63af23eed5a08d3d4b04a8c6a7b03bb7bc
jnguyen1098 <jnguyen1098@gmail.com>
2021-04-20 05:19:44 -0400

    comments

--- 1_1_hello_world/hello_world.c
+++ 1_1_hello_world/hello_world.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
-main()
+main()  // lack of `int` at the beginning causes a warning with `gcc`
 {
     printf("hello, ");
     printf("world");
     printf("\n");
+
+    // lack of `return` statement doesn't cause a warning though
 }
